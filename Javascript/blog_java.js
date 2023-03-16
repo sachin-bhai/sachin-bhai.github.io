350fa0feb1057babf8ff56cd21eda5ef12a23d43
sachin-bhai <sachin.shreekumar@gmail.com>
2023-03-16 21:24:38 +0530

    second commit,almost done

---
+++ Javascript/blog_java.js
@@ -0,0 +1,6 @@
+document.getElementById("next").addEventListener("click", function() {
+    window.location.href = "/home/sachin/blog_design/blogs/blog1.html"
+});
+document.getElementById("home").addEventListener("click",function() {
+    window.location.href ="/home/sachin/blog_design/main.html";
+});
--- Javascript/button.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("prev").addEventListener("click", function() {
-    window.location.href = "/home/sachin/blog_design/main.html";
-});
